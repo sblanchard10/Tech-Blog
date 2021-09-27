c543cb4f5a780e8af0cc273667257bc57f322643
Stephanie Blanchard <sblanchard2432@gmail.com>
2021-09-26 22:47:25 -0400

    added db and models folders

---
+++ models/User.js
@@ -0,0 +1,74 @@
+const { Model, DataTypes } = require("sequelize");
+const bcrypt = require("bcrypt");
+const sequelize = require("../config/connection");
+
+class User extends Model {
+  checkPassword(loginPw) {
+    return bcrypt.compareSync(loginPw, this.password);
+  }
+}
+
+User.init(
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    username: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      unique: {
+        args: true,
+        message: "Username taken!",
+        fields: ["username"],
+      },
+    },
+    email: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      unique: {
+        args: true,
+        message:
+          "An account already exists with this email address. Please try to login.",
+        fields: [sequelize.fn("lower", sequelize.col("email"))],
+      },
+      validate: {
+        isEmail: {
+          args: true,
+          msg: "Please provide a valid email address.",
+        },
+      },
+    },
+    password: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        len: {
+          args: [6, 30],
+          msg: "Password should be between 6-30 characters.",
+        },
+      },
+    },
+  },
+  {
+    hooks: {
+      async beforeCreate(newUserData) {
+        newUserData.password = await bcrypt.hash(newUserData.password, 10);
+        return newUserData;
+      },
+      async beforeUpdate(updatedUserData) {
+        updatedUserData.password = await bcrypt.hash(newUserData, 10);
+        return updatedUserData;
+      },
+    },
+    sequelize,
+    timestamps: false,
+    freezeTableName: true,
+    underscored: true,
+    modelName: "user",
+  }
+);
+
+module.exports = User;
