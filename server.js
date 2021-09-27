69cfaeb0d38c94d67e1977d0dc3535fecb343d6a
Stephanie Blanchard <sblanchard2432@gmail.com>
2021-09-26 23:35:10 -0400

    added server.js file

---
+++ server.js
@@ -0,0 +1,42 @@
+const express = require("express");
+const path = require("path");
+const routes = require("./controllers/");
+const sequelize = require("./config/connection");
+const helpers = require("./utils/helpers");
+
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+const exphbs = require("express-handlebars");
+const hbs = exphbs.create({ helpers });
+
+const session = require("express-session");
+const SequelizeStore = require("connect-session-sequelize")(session.Store);
+
+const sess = {
+  secret: process.env.SECRET,
+  cookie: { maxAge: 8 * 60 * 60 * 1000 }, // session expires after 8 hours
+  resave: false,
+  rolling: true,
+  saveUninitialized: true,
+  store: new SequelizeStore({
+    db: sequelize,
+  }),
+};
+
+app.engine("handlebars", hbs.engine);
+app.set("view engine", "handlebars");
+
+app.use(session(sess));
+app.use(express.static(path.join(__dirname, "public")));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use(routes);
+
+// turn on connection to db and server
+sequelize.sync({ force: true }).then(() => {
+  app.listen(PORT, () =>
+    console.log(`Now listening at http://localhost:${PORT}`)
+  );
+});
