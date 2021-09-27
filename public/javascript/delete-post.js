cf03df6b43ff34496c538a65c666dae8cbac1b61
Stephanie Blanchard <sblanchard2432@gmail.com>
2021-09-26 22:59:38 -0400

    added javascript and stylesheets folders

---
+++ public/javascript/delete-post.js
@@ -0,0 +1,25 @@
+async function deleteFormHandler(event) {
+  event.preventDefault();
+  console.log(event.target);
+
+  const postCard = event.target.parentElement;
+  const id = postCard.getAttribute("data-post-id");
+
+  const response = await fetch(`/api/posts/${id}`, {
+    method: "DELETE",
+  });
+  if (response.ok) {
+    document.location.replace("/dashboard");
+  } else {
+    // otherwise, get the data from the response and surface the error to the user
+    response.json().then((data) => {
+      const latestError = data.message;
+      document.querySelector(`#post-${id}-error`).textContent = latestError;
+    });
+  }
+}
+
+const deleteBtns = document.querySelectorAll(".delete-post-btn");
+deleteBtns.forEach((btn) => {
+  btn.addEventListener("click", deleteFormHandler);
+});
